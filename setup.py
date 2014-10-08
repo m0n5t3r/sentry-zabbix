f5164badc2a7df5a92823aaec6f931ecc7a899fa
Sabin Iacob <iacobs@m0n5t3r.info>
2014-10-08 18:29:15 +0300

    bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ f.close()
 
 setup(
     name='sentry-zabbix',
-    version='0.0.6',
+    version='0.0.7',
     author='Sabin Iacob',
     author_email='iacobs+pypi@gmail.com',
     url='http://github.com/m0n5t3r/sentry-zabbix',
